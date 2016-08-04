0065ae6121bac3494767d491dcdcf08b46db75d6
ehzShelter <emrulhasanzawad@outlook.com>
2016-08-05 02:22:12 +0600

    finding determinant with mutual recursion

--- Utilities.hpp
+++ Utilities.hpp
@@ -1,116 +1,77 @@
 #ifndef _UTILITIES_HPP_
 #define _UTILITIES_HPP_
 
-#include <vector>
 #include <iostream>
+#include <vector>
 
-namespace util{
+namespace util {
 
-template <typename ScalarType>
-using Matrix = std::vector<std::vector<ScalarType>>;
-/*
-    returns matrix
-*/
-template <typename ScalarType>
-Matrix<ScalarType> makeMatrix(size_t rows, size_t cols)
+template <typename T>
+using Matrix = std::vector<std::vector<T>>;
+
+template <typename T>
+Matrix<T> makeMatrix(size_t rows, size_t cols)
 {
-    Matrix<ScalarType> tmp(rows);
-    for (auto& row : tmp)
-    {
+    Matrix<T> temp(rows);
+    for (auto& row : temp) {
         row.resize(cols);
     }
 
-    return tmp;
+    return temp;
 }
-/*
-    since we can't compare floating point numbers to 0 (zero),
-    we'll check if they're in range close enough to zero
-    (-epsilon, epsilon), where default epsilon=0,000000000001
-*/
-template <typename ScalarType>
-bool inline isZero(ScalarType number, double epsilon = 1e-12)
+
+// If number is clouser enough to zero then like 0.00000000000001 set defaults to zero
+
+template <typename T>
+bool inline isZero(T number, double epsilon = 1e-12)
 {
     return (number < epsilon) && (number > -epsilon);
 }
-/*
-    simple function for printing Matrix content to given output
-    console is the default output
-    printMatrix(M) -> prints Matrix M in the console
-    printMatrix(M, std::ofstream("tmp.txt")) -> print Matrix M in tmp.txt file
-*/
-template <typename ScalarType>
-void inline printMatrix(const Matrix<ScalarType>& matrix, std::ostream& output=std::cout)
+
+// simple function for printing Matrix content to given output
+// console is the default output
+// printMatrix(M) -> prints Matrix M in the console
+// printMatrix(M, std::ofstream("temp.txt")) -> print Matrix M in temp.txt file
+// SIMILAR for read input
+
+template <typename T>
+void inline printMatrix(const Matrix<T>& matrix, std::ostream& output = std::cout)
 {
-    for (const auto& row : matrix)
-    {
-        for (const auto& col : row)
-        {
+    for (const auto& row : matrix) {
+        for (const auto& col : row) {
             output << col << " ";
         }
         output << std::endl;
     }
 }
-/*
-    simple function for populating Matrix from given input
-    keyboard is the default input
-    printMatrix(M) -> reads input from keyboard
-    printMatrix(M, std::ifstream("tmp.txt")) -> reads Matrix M from tmp.txt file
-*/
-template <typename ScalarType>
-void inline readMatrix( Matrix<ScalarType>& matrix, std::istream& input = std::cin)
+
+template <typename T>
+void inline readMatrix(Matrix<T>& matrix, std::istream& input = std::cin)
 {
-    for (auto& row : matrix)
-    {
-        for (auto& col : row)
-        {
+    for (auto& row : matrix) {
+        for (auto& col : row) {
             input >> col;
         }
     }
 }
 
-/*
-    function for for swapping rows if given coefficient (matrix[row][col]) is equal to zero
-    return true if there was non-zero coefficient below [row] in [column] and swap was succesful
-    false otherwise
-*/
-template <typename ScalarType>
-bool inline swapRows(util::Matrix<ScalarType>& matrix, size_t row, size_t col)
-{
-    for (size_t i = row + 1; i < matrix.size(); ++i)
-    {
-        if (!isZero(matrix[i][col]))
-        {
-            // found appropriate row -> swap rows and report success (true)
-            std::swap(matrix[row], matrix[i]);
-            return true;
-        }
-    }
-
-    return false; //failed to find appropriate row to swap with
-}
-
-/*
-    function for finding column where 'value' repeats the most
-    By default it seeks the column with the most amount of zeros
-*/
-template <typename ScalarType>
-size_t inline findBestColumn(util::Matrix<ScalarType>& M, ScalarType value = 0)
+//  function for finding column where 'value' repeats the most
+// By default it seeks the column with the most amount of zeros
+template <typename T>
+size_t inline findBestColumn(util::Matrix<T>& M, T value = 0)
 {
     // first = column index
     // second = number of 'value' occurrences
     std::pair<size_t, size_t> bestColumn{ 0, 0 };
 
-    for (size_t i = 0; i < M.size(); ++i)
-    {
+    for (size_t i = 0; i < M.size(); ++i) {
         size_t count{};
-        for (size_t j = 0; j < M.size(); ++j)
-        {
+        for (size_t j = 0; j < M.size(); ++j) {
             if (M[j][i] == value)
                 count++;
         }
 
-        if (count > bestColumn.second)
-        {
+        if (count > bestColumn.second) {
             bestColumn.first = i;
             bestColumn.second = count;
         }
@@ -118,24 +79,21 @@ size_t inline findBestColumn(util::Matrix<ScalarType>& M, ScalarType value = 0)
 
     return bestColumn.first;
 }
-/*
-    deletes row [i] and column [j] from matrix M
-*/
-template <typename ScalarType>
-util::Matrix<ScalarType> deleteRowAndColumn(util::Matrix<ScalarType> M, size_t i, size_t j)
+
+template <typename T>
+util::Matrix<T> deleteRowAndColumn(util::Matrix<T> M, size_t i, size_t j)
 {
-    // delete column j
-    for (size_t k = 0; k < M.size(); ++k)
-    {
+    // delete column ...as it is pointer to pointer
+    for (size_t k = 0; k < M.size(); ++k) {
         M[k].erase(M[k].begin() + j);
     }
-    // delete row i
+
+    // pointer to that row
     M.erase(M.begin() + i);
 
     return M;
 }
-
-
+// namespace End
 }
 
-#endif // end of header guard
+#endif
--- main.cpp
+++ main.cpp
@@ -6,21 +6,19 @@
 
 typedef double mType;
 
-template <typename ScalarType>
-ScalarType laplaceExpansion(util::Matrix<ScalarType> M);
-
-template <typename ScalarType>
-ScalarType findMinor(util::Matrix<ScalarType>& M, size_t row, size_t col);
-
-template <typename ScalarType>
-ScalarType expand(util::Matrix<ScalarType>& M, size_t col);
+// prototypes
+template <typename T>
+T laplaceExpansion(util::Matrix<T> M);
+template <typename T>
+T findMinor(util::Matrix<T>& M, size_t row, size_t col);
+template <typename T>
+T expand(util::Matrix<T>& M, size_t col);
 
 int main(int argc, char* argv[])
 {
     const std::string msg = "Proper usage: \"MatrixDeterminant -f filename\" ";
 
-    if (argc == 3) // == MatrixDeterminant x y
-    {
+    if (argc == 3) {
         if (std::string(argv[1]) == "-f") {
             std::ifstream inputFile(argv[2]);
 
@@ -37,6 +35,7 @@ int main(int argc, char* argv[])
 
                 std::cout << "M = " << std::endl;
                 util::printMatrix(matrix);
+
                 std::ofstream tmp("results.txt", std::ios_base::app);
                 tmp << "M = " << std::endl;
                 util::printMatrix(matrix, tmp);
@@ -51,7 +50,6 @@ int main(int argc, char* argv[])
             else {
                 std::cerr << "Couldn't open file : " << argv[2];
             }
-
         }
         // wrong first argument -> show error message
         else {
@@ -61,46 +59,47 @@ int main(int argc, char* argv[])
         //@ todo = proccess keyboard input
         std::cerr << msg;
     }
-
-    return 0;
 }
 
-template <typename ScalarType>
-ScalarType laplaceExpansion(util::Matrix<ScalarType> M)
+template <typename T>
+T laplaceExpansion(util::Matrix<T> M)
 {
-    ScalarType determinant{};
+    T determinant{};
 
-    if (M.size() == 1)
+    if (M.size() == 1) {
         determinant = M[0][0];
-    else if (M.size() == 2)
+    } else if (M.size() == 2) {
         determinant = (M[0][0] * M[1][1]) - (M[0][1] * M[1][0]);
-    else {
+    } else {
         determinant = expand(M, util::findBestColumn(M));
     }
 
     return determinant;
 }
-template <typename ScalarType>
-ScalarType expand(util::Matrix<ScalarType>& M, size_t col)
+
+template <typename T>
+T expand(util::Matrix<T>& M, size_t col)
 {
-    ScalarType determinant{};
-    ScalarType sign{ 1 };
+    T determinant{};
+    T sign{ 1 };
 
-    for (size_t i = 0; i < M.size(); ++i) {
-        if ((i + col) % 2 == 1)
+    for (size_t row = 0; row < M.size(); ++row) {
+        if ((row + col) % 2 == 1) {
             sign = -1;
-        else
+        } else {
             sign = 1;
+        }
 
-        if (!util::isZero(M[i][col]))
-            determinant += sign * M[i][col] * findMinor(M, i, col);
+        if (!util::isZero(M[row][col])) {
+            determinant += sign * M[row][col] * findMinor(M, row, col);
+        }
     }
 
     return determinant;
 }
 
-template <typename ScalarType>
-ScalarType findMinor(util::Matrix<ScalarType>& M, size_t row, size_t col)
+template <typename T>
+T findMinor(util::Matrix<T>& M, size_t row, size_t col)
 {
     return laplaceExpansion(util::deleteRowAndColumn(M, row, col));
 }
