eda8ce0fdce1951b7bd090861a508a22b1057be7
ehzShelter <emrulhasanzawad@outlook.com>
2016-08-05 23:28:23 +0600

    new class implemented

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "Utilities.hpp"
+#include "hillchiper.hpp"
 #include <algorithm>
 #include <fstream>
 #include <iostream>
@@ -6,16 +7,10 @@
 
 typedef double mType;
 
-// prototypes
-template <typename T>
-T laplaceExpansion(util::Matrix<T> M);
-template <typename T>
-T findMinor(util::Matrix<T>& M, size_t row, size_t col);
-template <typename T>
-T expand(util::Matrix<T>& M, size_t col);
 
 int main(int argc, char* argv[])
 {
+    Hill chiper;
     const std::string msg = "Proper usage: \"MatrixDeterminant -f filename\" ";
 
     if (argc == 3) {
@@ -41,7 +36,7 @@ int main(int argc, char* argv[])
                 util::printMatrix(matrix, tmp);
 
                 // LAPLACE EXPANSION
-                determinant = laplaceExpansion(matrix);
+                determinant = chiper.laplaceExpansion(matrix);
                 std::cout << "LaplaceExpansion::det(M) = " << determinant << std::endl;
                 tmp << "LaplaceExpansion::det(M) = " << determinant << std::endl;
 
@@ -61,45 +56,3 @@ int main(int argc, char* argv[])
     }
 }
 
-template <typename T>
-T laplaceExpansion(util::Matrix<T> M)
-{
-    T determinant{};
-
-    if (M.size() == 1) {
-        determinant = M[0][0];
-    } else if (M.size() == 2) {
-        determinant = (M[0][0] * M[1][1]) - (M[0][1] * M[1][0]);
-    } else {
-        determinant = expand(M, util::findBestColumn(M));
-    }
-
-    return determinant;
-}
-
-template <typename T>
-T expand(util::Matrix<T>& M, size_t col)
-{
-    T determinant{};
-    T sign{ 1 };
-
-    for (size_t row = 0; row < M.size(); ++row) {
-        if ((row + col) % 2 == 1) {
-            sign = -1;
-        } else {
-            sign = 1;
-        }
-
-        if (!util::isZero(M[row][col])) {
-            determinant += sign * M[row][col] * findMinor(M, row, col);
-        }
-    }
-
-    return determinant;
-}
-
-template <typename T>
-T findMinor(util::Matrix<T>& M, size_t row, size_t col)
-{
-    return laplaceExpansion(util::deleteRowAndColumn(M, row, col));
-}
