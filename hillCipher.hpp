be51500c8e3f93040e08d063fd0c0aeed2a090ba
ehzShelter <emrulhasanzawad@outlook.com>
2016-08-10 21:21:20 +0600

    SOLID is good design pattern....actually design patter is relative... there is nothing absollute

--- hillCipher.hpp
+++ hillCipher.hpp
@@ -15,69 +15,70 @@ private:
 
 public:
     // setter and getter of string
-    void setEncryptText(void)
-    {
-        termios process = linuxUtil::setNotEchoingMode();
-        this->encryptText = ValidateInput::inputData("message", ".+");
-        linuxUtil::goBack(process);
-    }
-
+    void setEncryptText(void);
     // get encryptText text
-    std::string getEncryptText(void)
-    {
-        return this->encryptText;
-    }
+    std::string getEncryptText(void);
+    void splittingOnTheFly(MathUtility::VV twoD, std::string& vec, size_t tokens, size_t split);
 
-
-    void splittingOnTheFly(MathUtility::VV twoD, std::string& vec, size_t tokens, size_t split)
-    {
-        for (size_t i = 0; i < tokens; i++) {
-            std::copy_n(vec.begin(), split, std::back_inserter(twoD[i]));
-
-            MathUtility::dimensionVariant(twoD[i], split);
-
-            vec.erase(vec.begin(), vec.begin() + split);
-        }
-    }
-
-    void tokeniner(std::string& vec)
-    {
-        size_t tokens{};
-        size_t spaceFact{};
-
-        if (vec.size() % 2 == 0) {
-            tokens = vec.size() / 2;
-            spaceFact = vec.size();
-        } else if ((vec.size() % 2 != 0) && MathUtility::isPrime(vec.size())) {
-            vec.resize(vec.size() + 1);
-            tokens = (vec.size() / 2);
-            spaceFact = vec.size();
-        } else {
-            tokens = vec.size() / 3;
-            spaceFact = vec.size();
-        }
-
-        std::cout << "TOkensize : " << tokens << std::endl;
-
-        size_t split = (spaceFact % 2 == 0) ? 2 : 3;
-
-        MathUtility::VV twoD(tokens);
-
-        splittingOnTheFly(twoD, vec, tokens, split);
-    }
-
-    void statementToken()
-    {
-        try {
-
-            std::string vec = this->getEncryptText();
-            tokeniner(vec);
-
-        } catch (...) {
-            std::cout << "Something wrong there ! " << std::endl;
-        }
-    }
-
+    void tokeniner(std::string& vec);
+    void statementToken();
 };
+
+// Implementation of hill class
+void Hill::setEncryptText(void)
+{
+    termios process = linuxUtil::setNotEchoingMode();
+    this->encryptText = ValidateInput::inputData("message", ".+");
+    linuxUtil::goBack(process);
+}
+
+// get encryptText text
+std::string Hill::getEncryptText(void)
+{
+    return this->encryptText;
+}
+
+void Hill::splittingOnTheFly(MathUtility::VV twoD, std::string& vec, size_t tokens, size_t split)
+{
+    for (size_t i = 0; i < tokens; i++) {
+        std::copy_n(vec.begin(), split, std::back_inserter(twoD[i]));
+        MathUtility::dimensionVariant(twoD[i], split);
+        vec.erase(vec.begin(), vec.begin() + split);
+    }
+}
+
+void Hill::tokeniner(std::string& vec)
+{
+    size_t tokens{};
+    size_t spaceFact{};
+
+    if (vec.size() % 2 == 0) {
+        tokens = vec.size() / 2;
+        spaceFact = vec.size();
+    } else if ((vec.size() % 2 != 0) && MathUtility::isPrime(vec.size())) {
+        vec.resize(vec.size() + 1);
+        tokens = (vec.size() / 2);
+        spaceFact = vec.size();
+    } else {
+        tokens = vec.size() / 3;
+        spaceFact = vec.size();
+    }
+
+    std::cout << "TOkensize : " << tokens << std::endl;
+    size_t split = (spaceFact % 2 == 0) ? 2 : 3;
+    MathUtility::VV twoD(tokens);
+
+    splittingOnTheFly(twoD, vec, tokens, split);
+}
+
+void Hill::statementToken()
+{
+    try {
+        std::string vec = this->getEncryptText();
+        tokeniner(vec);
+    } catch (...) {
+        std::cout << "Something wrong there ! " << std::endl;
+    }
+}
 
 #endif
