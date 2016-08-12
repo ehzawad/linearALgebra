0af44ceab6a4ea4f672507317815193e0e12283c
ehzShelter <emrulhasanzawad@outlook.com>
2016-08-12 22:52:03 +0600

    reducing main unnessary comment

--- main.cpp
+++ main.cpp
@@ -5,29 +5,16 @@
 #include <stdexcept>
 #include <string>
 
-typedef double mType;
-
 int main(void)
 {
     try {
-        // MathUtility::VV matrix = MathUtility::makeMatrix(3, 3);
-        // //
-        // MathUtility::readMatrix(matrix, std::cin);
-        //
-        // MathUtility::VV F = MathUtility::findInverseMat(matrix);
-        //
-        // MathUtility::printMatrix(F);
-
         Hill Cipher;
 
         Cipher.statementToken();
-
         std::cout << std::endl;
 
         Cipher.decryptCode();
         std::cout << std::endl;
-
-        std::flush(std::cout);
     } catch (...) {
         std::cout << "Something unexpected !" << std::endl;
     }
