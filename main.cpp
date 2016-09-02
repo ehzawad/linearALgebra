f590daaa1ba1178f10a71658a5b4042021bd2dad
ehzShelter <emrulhasanzawad@outlook.com>
2016-09-02 18:19:26 +0600

    pretending calculation takes 2 seconds

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <stdexcept>
 #include <string>
+#include <chrono>
+#include <thread>
 
 int main(void)
 {
@@ -12,6 +14,11 @@ int main(void)
         // encryption
         Cipher.doEncrypt();
         Cipher.printEncryptedCode();
+        // C++11 threading
+        // pause before continuing
+        // http://en.cppreference.com/w/cpp/thread/sleep_for
+        // The process thread will hold for 1 seconds
+        std::this_thread::sleep_for(std::chrono::seconds(2));
 
         // decryption
         Cipher.onlyDecrypt();
